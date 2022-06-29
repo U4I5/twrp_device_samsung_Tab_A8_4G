355b9e0ab5a117721c499754d5cc1c1c4e71f4c3
U4I5 <u.soglondey@gmail.com>
2022-06-29 12:03:38 +0200

    Inital commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_gta8.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gta8-user \
+    omni_gta8-userdebug \
+    omni_gta8-eng
